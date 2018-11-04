9ee43f33c631eef766984a620b452babf9cde39a
1701c <alex@reventlow.net>
2018-11-04 15:55:35 -0600

    added GET request

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ var numRecords;
 var startYear;
 var endYear;
 
+var timesAPI = {
+  requestData: function (term, numRecords, startYear, endYear) {
+    console.log('requesting data');
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({'api-key': "d7702ba6246c41f19ef23e6ed45b0c05"});
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }) .then(function(response) {
+      console.log(response);
+      for (i = 0; i < response.length; i++)
+        titleDiv = $("<h3>");
+        urlDiv = $("<p>");
+        authorDiv = $("<p>");
+        yearDiv = $("<p>");
+        // urlDiv.append(response.docs[i].web_url)
+        console.log(response.docs[i]);
+  }).fail(function(err) {
+    throw err;
+    });
+  }
+}
 
 $(document).ready(function () {
   $('#searchButton').click(function () {
@@ -13,7 +35,7 @@ $(document).ready(function () {
     
     console.log(term + ' ' + numRecords + ' ' + startYear + ' ' + endYear);
 
-    // call to backend search function goes here
+    timesAPI.requestData(term, numRecords, startYear, endYear);
   })
 
   $('#clearButton').click(function () {
